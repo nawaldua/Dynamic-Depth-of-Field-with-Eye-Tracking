df24a595a98bfba9ec1589beef6bd816bd78551f
Suchith <SuchithSuresh@users.github.com>
2020-07-21 20:13:49 -0500

    Minor Changes, Improving Readability

--- code/main.py
+++ code/main.py
@@ -3,43 +3,46 @@ from tqdm import tqdm
 import tkinter as tk
 from tkinter import filedialog as fd
 import sys
-from guifuncts import browse_directory,view,preview
+from guifuncts import browse_directory, view, preview
 from application import application
 from iploader import read_inputs
-from blurproc import foccal,newmaskimg,renderopfast
+from blurproc import foccal, newmaskimg, renderopfast
 import cv2 as cv
 import globalvars
+
+
 def main():
-	#Set up GUI
-	globalvars.init()
-	window = tk.Tk()  #Makes main window
-	window.wm_title("Focus_GUI: Root Window")
-	window.config(background="#FFFFFF")
+    # Set up GUI
+    globalvars.init()
+    window = tk.Tk()  # Makes main window
+    window.wm_title("Focus_GUI: Root Window")
+    window.config(background="#FFFFFF")
 
-	print("lbl here")
-	globalvars.lbl1 = tk.StringVar()
-	print("tk here")
-	globalvars.lbl1.set("Choose the required Directory")
+    print("lbl here")
+    globalvars.lbl1 = tk.StringVar()
+    print("tk here")
+    globalvars.lbl1.set("Choose the required Directory")
 
-	# Button to choose Video directory with the .jpg and .npy files
-	print(globalvars.lbl1)
-	but_direc = tk.Button(window, text = "Directory", width = 10, command =browse_directory)
-	but_direc.grid(row=0, column=1)
+    # Button to choose Video directory with the .jpg and .npy files
+    print(globalvars.lbl1)
+    but_direc = tk.Button(window, text="Directory", width=10, command=browse_directory)
+    but_direc.grid(row=0, column=1)
 
-	# Label to dynamically display the current directory    
-	label1 = tk.Label(window, textvariable = globalvars.lbl1)
-	label1.grid(row=0, column=3)
+    # Label to dynamically display the current directory    
+    label1 = tk.Label(window, textvariable=globalvars.lbl1)
+    label1.grid(row=0, column=3)
 
-	but_preview = tk.Button(window, text = "Preview", width = 10, command=lambda: preview(window))
-	but_preview.grid(row=2, column=1)
+    but_preview = tk.Button(window, text="Preview", width=10, command=lambda: preview(window))
+    but_preview.grid(row=2, column=1)
 
-	but_view = tk.Button(window, text = "View Video", width = 10, command=lambda: view(window))
-	but_view.grid(row=4, column=1)
+    but_view = tk.Button(window, text="View Video", width=10, command=lambda: view(window))
+    but_view.grid(row=4, column=1)
 
-	but_quitApp = tk.Button(window, text = "Quit", width = 10, command = window.destroy)
-	but_quitApp.grid(row=6, column=1)
-	
-	window.mainloop()  #Starts GUI
-	
+    but_quitApp = tk.Button(window, text="Quit", width=10, command=window.destroy)
+    but_quitApp.grid(row=6, column=1)
+
+    window.mainloop()  # Starts GUI
+
+
 if __name__ == "__main__":
     main()
--- code/outputfuncs.py
+++ code/outputfuncs.py
@@ -2,10 +2,11 @@ import cv2 as cv
 import globalvars
 import numpy as np
 from iploader import read_inputs
-from blurproc import foccal,newmaskimg,renderopfast
+from blurproc import foccal, newmaskimg, renderopfast
 import time
-def mouse_move(event,x,y,flags, param):
-    
+
+
+def mouse_move(event, x, y, flags, param):
     if event == cv.EVENT_MOUSEMOVE:
         globalvars.posx = x
         globalvars.posy = y
@@ -15,32 +16,21 @@ def mouse_move(event,x,y,flags, param):
 def output_win():
     globalvars.posx = 0
     globalvars.posy = 0
-    
-    
+
     cv.namedWindow('output')
-    
-    cv.setMouseCallback('output',mouse_move)
-    
+
+    cv.setMouseCallback('output', mouse_move)
+
     # Moving across width is x value
     # Moving along height is y value
     # table[frame_index][row or y][col or x]
-    
-    frame = globalvars.opimarr[0][9]
     i = 0
     blur_area = 9
-    comp_var = 9
-    
-    font = cv.FONT_HERSHEY_SIMPLEX
-    fontScale = 1
-    color = (0, 0, 255)
-    thickness = 2
-    
-#     print(len(globalvars.imarr))
-        
-    while(i < len(globalvars.imarr)):
+
+    while (i < len(globalvars.imarr)):
 
         frame = globalvars.opimarr[i][blur_area]
-        cv.imshow('output',frame)        
+        cv.imshow('output', frame)
         time.sleep(0.04)
 
         i += 1
@@ -56,64 +46,52 @@ def output_win():
         if cv.waitKey(20) & 0xFF == 27:
             break
 
-
     cv.destroyAllWindows()
-    
+
     return
 
+
 def genpreview(blurfac):
-    
-    
     globalvars.img = cv.imread(globalvars.dirr + '/(01).jpg')
     globalvars.arr1 = np.load(globalvars.dirr + '/(01).npy')
-    
-    
+
     globalvars.imarr, npyarr = read_inputs()
-    
-    minar=np.min(npyarr[0])
-    step=np.round((np.max(npyarr[0])-np.min(npyarr[0]))/10,3)
+
+    minar = np.min(npyarr[0])
+    step = np.round((np.max(npyarr[0]) - np.min(npyarr[0])) / 10, 3)
 
     blarrs = []
     for i in range(10):
-        blarrs.append(foccal(minar+i*step, step, minar))
-        
-    
+        blarrs.append(foccal(minar + i * step, step, minar))
 
-    blurimages,masks, globalvars.lup_tab=newmaskimg(globalvars.imarr[0], npyarr[0], blurfac)
-    globalvars.opims=renderopfast(blurimages,blarrs,globalvars.imarr[0], masks)
+    blurimages, masks, globalvars.lup_tab = newmaskimg(globalvars.imarr[0], npyarr[0], blurfac)
+    globalvars.opims = renderopfast(blurimages, blarrs, globalvars.imarr[0], masks)
     return
+
 
 def preview_win():
     blurfac = int(globalvars.blurVariable.get())
-#     print(blurVariable.get())
+
     genpreview(blurfac)
-    
-    
+
     globalvars.posx = 0
     globalvars.posy = 0
-    
+
     cv.namedWindow('Preview output')
-    #cv.resizeWindow('output', 600,600)
-    
-    cv.setMouseCallback('Preview output',mouse_move)
-    
+
+    cv.setMouseCallback('Preview output', mouse_move)
+
     # Moving across width is x value
     # Moving along height is y value
     # table[frame_index][row or y][col or x]
-    i=0
+    i = 0
     blur_area = 9
-    comp_var = 9
-    font = cv.FONT_HERSHEY_SIMPLEX 
-    fontScale = 1
-    color = (0, 0, 255)
-    thickness = 2
-    
-    while(i == 0):
-            
+
+    while (i == 0):
+
         frame = globalvars.opims[blur_area]
-        cv.imshow('Preview output',frame)        
-#         time.sleep(0.04)
-            
+        cv.imshow('Preview output', frame)
+
         comp_var = np.uint8(globalvars.lup_tab[globalvars.posy][globalvars.posx])
 
         if comp_var < 10:
@@ -122,7 +100,6 @@ def preview_win():
         if cv.waitKey(20) & 0xFF == 27:
             break
 
-
     cv.destroyAllWindows()
-    
+
     return
